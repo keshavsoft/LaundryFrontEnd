d884c89edf8c705396068a29950acad28941b7eb
keshavsoft <nknnkumar@yahoo.com>
2024-08-30 18:18:45 +0530

    index working good

--- src/Clients/Washtex/Branch/KCode/generateVariables.js
+++ src/Clients/Washtex/Branch/KCode/generateVariables.js
@@ -1,22 +1,9 @@
-import fs from "fs";
-import { StartFunc as getColumnsData } from "./getColumnsData.js";
-import { StartFunc as GetTableNames } from "./GetTableNames.js";
-import { StartFunc as ReadDataSchema } from "./ReadDataSchema.js";
-
 import { StartFunc as prepareSideBar } from "./prepareSideBar.js";
-
-import { StartFunc as foreignTableColumnsConfig } from "./foreignTableColumnsConfig.js";
 
 const StartFunc = ({ mode, inFilesArray, inSrcPath }) => {
     const variables = {};
     let LocalFiles = inFilesArray;
-    // let LoopInsidecolumnData = getColumnsData({ inSrcPath, inTableName });
-    let LocalTableNames = GetTableNames();
-    //   const sidebarItems = fs.readFileSync(`${inSrcPath}/KCode/sideBarItems.json`, { encoding: 'utf8' });
     const sidebarItems = prepareSideBar();
-    // console.log("sidebarItems : ", sidebarItems);
-    const LocalTablesWithSchema = ReadDataSchema();
-    // console.log("LocalTablesWithSchema : ", LocalTableNames);
 
     Object.keys(LocalFiles).forEach((filename) => {
         variables[filename + '.html'] = {
@@ -26,22 +13,6 @@ const StartFunc = ({ mode, inFilesArray, inSrcPath }) => {
             isDev: mode === 'development',
             tableName: "",
             columnData: {}
-        };
-
-        let LoopInsideTableName = LocalTableNames.find(element => {
-            return filename.startsWith(element);
-        });
-
-        if (LoopInsideTableName === undefined === false) {
-            let LoopInsidecolumnData = getColumnsData({ inSrcPath, inTableName: LoopInsideTableName });
-            console.log("LoopInsidecolumnData: ", LoopInsidecolumnData);
-            variables[filename + '.html'].tableName = LoopInsideTableName;
-            variables[filename + '.html'].columnData = LoopInsidecolumnData;
-
-            let LocalInsideForeignTable = foreignTableColumnsConfig({ inTableName: LoopInsideTableName, inTableWithColumnData: LocalTablesWithSchema });
-
-            variables[filename + '.html'].subTableName = LocalInsideForeignTable?.tableName;
-            variables[filename + '.html'].foreignTablecolumnData = LocalInsideForeignTable?.ColumnInfo;
         };
     });
 
--- src/Clients/Washtex/Branch/KCode/prepareSideBar.js
+++ src/Clients/Washtex/Branch/KCode/prepareSideBar.js
@@ -1,9 +1,6 @@
-import fs from "fs";
-import path from 'path';
 import sideBarItemsJson from './sideBarItems.json' with {type: 'json'};
 
 const StartFunc = () => {
-    console.log("LocalSideBarArray : ", sideBarItemsJson);
     return sideBarItemsJson;
 };
 
