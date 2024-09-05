3b3c7f40f1e7b0031be334c7eaac67af10d51f35
sharath0036 <ksharathbabu2@gmail.com>
2024-09-05 11:24:08 +0530

    making  modificarions in QRRaise all

--- src/Clients/BranchToday/Branch/SourceCode/Js/pages/TodayQrRaiseAll/FormLoad/AddListeners/RefreshBSTableId/ButtonClickFunc.js
+++ src/Clients/BranchToday/Branch/SourceCode/Js/pages/TodayQrRaiseAll/FormLoad/AddListeners/RefreshBSTableId/ButtonClickFunc.js
@@ -1,18 +1,14 @@
-import { StartFunc as Receipts } from "./Receipts/Entry.js";
+import { StartFunc as Orders } from "./Orders/GetFetch.js";
 
 import { StartFunc as StartFuncAfterFetch } from "./AfterFetch/EntryFile.js";
 
 let StartFunc = async () => {
-    try {
-        let [a] = await Promise.all([Receipts()]);
 
-        jVarGlobalPresentViewData = [...a];
+    let a = await Orders();
+    jVarGlobalPresentViewData = a;
 
-        StartFuncAfterFetch();
-    } catch (e) {
-        console.log("error from Promise all : ", e);
-        alert("error from Promise All");
-    };
+    StartFuncAfterFetch();
+
 };
 
 export { StartFunc }
