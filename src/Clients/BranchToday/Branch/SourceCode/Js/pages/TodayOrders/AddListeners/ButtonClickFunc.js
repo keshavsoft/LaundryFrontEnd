c28e045040a1652ca7bdff8097086651dcea5d8e
pushpadamalanka82 <pushpadamalanka@gmail.com>
2024-09-05 19:53:44 +0530

    new order

---
+++ src/Clients/BranchToday/Branch/SourceCode/Js/pages/TodayOrders/AddListeners/ButtonClickFunc.js
@@ -0,0 +1,7 @@
+import { StartFunc as StartFuncFetchFromFuncs } from "./RefreshBSTableId/FetchFromFuncs/Entry.js";
+
+const StartFunc = () => {
+    StartFuncFetchFromFuncs();
+};
+
+export { StartFunc }
