95d106a5d148613590c9dac8225d9627b17a3d5d
Srivasu215 <sri.vasu215@gmail.com>
2024-09-21 21:04:14 +0530

    Branch Dc

--- src/Clients/ToFactory/Branch/SourceCode/Js/pages/ToFactoryQRPrint/ShowOnDom.js
+++
@@ -1,8 +0,0 @@
-let StartFunc = async () => {
-    let jVarLocalHtmlId = 'RefreshBSTableId';
-    let jVarLocalRefreshBSTableId = document.getElementById(jVarLocalHtmlId);
-    jVarLocalRefreshBSTableId.click();
-   
-};
-
-export { StartFunc }
