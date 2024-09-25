f2debbc2cfbfbbc661125682edcf0b960ab65228
Pavansai <pavansai1151@gmail..com>
2024-09-25 16:14:59 +0530

    fixed savebutton on addons

--- src/Clients/NewOrders/Branch/SourceCode/Js/pages/AddItems/FormLoad/BuildBSTables/AddOnTableOnModal/OnLoadSuccess/AddOnModalSaveButtonId/FetchFromFuncs/PostFetch.js
+++ src/Clients/NewOrders/Branch/SourceCode/Js/pages/AddItems/FormLoad/BuildBSTables/AddOnTableOnModal/OnLoadSuccess/AddOnModalSaveButtonId/FetchFromFuncs/PostFetch.js
@@ -1,4 +1,5 @@
 import { StartFunc as StartFuncFetchHeaders } from "./FetchHeaders/EntryFile.js";
+import ConfigJson from "../../../../../../Config.json" with {type:'json'};
 
 let StartFunc = async ({ PrepareBody }) => {
     let jVarLocalFetchHeaders = StartFuncFetchHeaders({ PrepareBody });
@@ -6,7 +7,7 @@ let StartFunc = async ({ PrepareBody }) => {
     let jVarLocalMainRowpk = document.getElementById("OrderNumberId").innerHTML;
 
     // let jVarLocalFetchUrl = `/Custom/Clients/Washtex/Orders/NewOrder/AddAddOn/${jVarLocalBranchName}`;
-    let jVarLocalFetchUrl = `/binV3/${jVarLocalBranchName}/SubTable/Create/${jVarLocalMainRowpk}/AddOnData`;
+    let jVarLocalFetchUrl = `/${ConfigJson.routePath}/${jVarLocalBranchName}/SubTable/Create/${jVarLocalMainRowpk}/AddOnData`;
     let response = await fetch(jVarLocalFetchUrl, jVarLocalFetchHeaders);
 
     return await response;
