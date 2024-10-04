f7827943b6f70a53470fcab267d76900431abb57
sharath0036 <ksharathbabu2@gmail.com>
2024-10-04 12:41:24 +0530

    pagenition and urls

--- src/Clients/Today/QrCodes/SourceCode/Js/pages/QrAll/StartFunc.js
+++ src/Clients/Today/QrCodes/SourceCode/Js/pages/QrAll/StartFunc.js
@@ -1,11 +1,11 @@
 import { StartFunc as StartFuncShowOnDom } from "./ShowOnDom.js";
 import { StartFunc as StartFuncFormLoad } from "./FormLoad/StartFunc.js";
-import { StartFunc as LoginModal } from "../../../assets/static/js/Common/NiceAdmin/Header/LoginModal/EntryFile.js";
+// import { StartFunc as LoginModal } from "../../../assets/static/js/Common/NiceAdmin/Header/LoginModal/EntryFile.js";
 
 const StartFunc = () => {
     StartFuncFormLoad();
-    let jVarLocalFromAdmin = LoginModal({ inSuccessFunc: StartFuncShowOnDom });
-
+    // let jVarLocalFromAdmin = LoginModal({ inSuccessFunc: StartFuncShowOnDom });
+    let jVarLocalFromAdmin = true;
     if (jVarLocalFromAdmin) {
         StartFuncShowOnDom().then();
     };
