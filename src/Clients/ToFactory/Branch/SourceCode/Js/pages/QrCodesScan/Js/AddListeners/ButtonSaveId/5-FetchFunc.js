21b2e5699c46f5fb16b2ac72f0f7e0422ac03e54
sharath0036 <ksharathbabu2@gmail.com>
2024-10-09 12:54:01 +0530

    fetching urls

--- src/Clients/ToFactory/Branch/SourceCode/Js/pages/QrCodesScan/Js/AddListeners/ButtonSaveId/5-FetchFunc.js
+++ src/Clients/ToFactory/Branch/SourceCode/Js/pages/QrCodesScan/Js/AddListeners/ButtonSaveId/5-FetchFunc.js
@@ -1,9 +1,9 @@
 import { StartFunc as StartFuncFetchHeaders } from "./FetchHeaders/EntryFile.js";
-
+import ConfigJson from './Config.json' with {type: 'json'};
 let StartFunc = async () => {
     let jVarLocalFetchHeaders = StartFuncFetchHeaders();
     let jVarLocalbranchName = localStorage.getItem("BranchName")
-    let jVarLocalFetchUrl = `/Custom/Clients/Laundry/Qrcodes/EntryScan/${jVarLocalbranchName}`;
+    let jVarLocalFetchUrl = `${ConfigJson.url}/${jVarLocalbranchName}`;
     let response = await fetch(jVarLocalFetchUrl, jVarLocalFetchHeaders);
     let data = await response.json();
 
