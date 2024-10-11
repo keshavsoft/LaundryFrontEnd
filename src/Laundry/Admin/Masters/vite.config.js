7e5a8fb16a2cf08133a767b1f950b1a343210024
pushpadamalanka82 <pushpadamalanka@gmail.com>
2024-10-11 11:07:28 +0530

    new ui

--- src/Laundry/Admin/Masters/vite.config.js
+++ src/Laundry/Admin/Masters/vite.config.js
@@ -12,7 +12,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const SourceFolderName = "SourceCode";
-const SrcFolder = "src/Laundry/Washtex/Masters";
+const SrcFolder = "src/Laundry/Admin/Masters";
 
 const FrontEndDistFolder = "publicDir/Admin/Masters";
 
