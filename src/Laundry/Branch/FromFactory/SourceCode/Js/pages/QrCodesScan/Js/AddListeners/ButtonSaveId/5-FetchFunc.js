5e90bc9f7b6dfc8319a2ab5cb2b0bf575d516aee
Srivasu215 <sri.vasu215@gmail.com>
2024-10-23 17:13:43 +0530

    From Factory

--- src/Laundry/Branch/FromFactory/SourceCode/Js/pages/QrCodesScan/Js/AddListeners/ButtonSaveId/5-FetchFunc.js
+++ src/Laundry/Branch/FromFactory/SourceCode/Js/pages/QrCodesScan/Js/AddListeners/ButtonSaveId/5-FetchFunc.js
@@ -4,7 +4,7 @@ let StartFunc = async () => {
     let jVarLocalFetchHeaders = StartFuncFetchHeaders();
 
     let jVarLocalbranchName = localStorage.getItem("BranchName")
-    let jVarLocalFetchUrl = `/Custom/Clients/Laundry/Branch/FromFactory/Scan/QrCode/${jVarLocalbranchName}`;
+    let jVarLocalFetchUrl = `/Custom/Cleaning/Branch/Factory/FromFactory/Scan/QrCode/${jVarLocalbranchName}`;
 
     let response = await fetch(jVarLocalFetchUrl, jVarLocalFetchHeaders);
     let data = await response.json();
