d5ed5453c0152058653779c2a93c7fac9454ea57
pushpadamalanka82 <y*********@gmail.com>
2024-10-23 19:50:19 +0530

    endpoints

--- src/Laundry/Branch/FromFactoryQrCodes/SourceCode/Js/pages/QrshowAll/FormLoad/BuildBsTable/EntryFile.js
+++ src/Laundry/Branch/FromFactoryQrCodes/SourceCode/Js/pages/QrshowAll/FormLoad/BuildBsTable/EntryFile.js
@@ -11,7 +11,7 @@ const StartFunc = () => {
     optionsJson.queryParams = queryParams;
     optionsJson.responseHandler = responseHandler;
     let LocalFactoryName = localStorage.getItem("FactoryName");
-    optionsJson.url=`/Custom/Clients/Laundry/Factory/Inward/Scan/QrCode/${LocalFactoryName}`;
+    optionsJson.url=`/Custom/Cleaning/Branch/Factory/FromFactory/Scan/QrCode/${LocalFactoryName}`;
     $table.bootstrapTable(optionsJson);
 };
 
