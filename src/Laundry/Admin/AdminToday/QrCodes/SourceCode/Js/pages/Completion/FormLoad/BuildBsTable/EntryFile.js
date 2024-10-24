005cf2d5aa33fb34cfe7dee4fe76b3f72bd399c0
pushpadamalanka82 <y*********@gmail.com>
2024-10-24 11:03:37 +0530

    serials

--- src/Laundry/Admin/AdminToday/QrCodes/SourceCode/Js/pages/Completion/FormLoad/BuildBsTable/EntryFile.js
+++ src/Laundry/Admin/AdminToday/QrCodes/SourceCode/Js/pages/Completion/FormLoad/BuildBsTable/EntryFile.js
@@ -1,13 +1,13 @@
 import optionsJson from './options.json' with {type: 'json'};
-import { StartFunc as StartFuncOnClickRowFunc } from "./onClickRow/EntryFile.js";
+
+import { StartFunc as ForColumns } from "./ForColumns/entryFile.js";
 
 const StartFunc = () => {
     var $table = $('#table');
 
-    optionsJson.onClickRow = StartFuncOnClickRowFunc;
+    ForColumns({ inColumns: optionsJson.columns });
 
     $table.bootstrapTable(optionsJson);
 };
 
 export { StartFunc };
-
