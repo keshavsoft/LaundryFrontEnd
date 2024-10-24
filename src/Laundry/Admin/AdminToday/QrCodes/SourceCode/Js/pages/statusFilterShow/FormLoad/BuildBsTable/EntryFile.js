6b98eeebbd4f629fdcc7efdc1a5342e7a7847c09
sharath0036 <ksharathbabu2@gmail.com>
2024-10-24 12:40:52 +0530

    making functions global to local

--- src/Laundry/Admin/AdminToday/QrCodes/SourceCode/Js/pages/statusFilterShow/FormLoad/BuildBsTable/EntryFile.js
+++ src/Laundry/Admin/AdminToday/QrCodes/SourceCode/Js/pages/statusFilterShow/FormLoad/BuildBsTable/EntryFile.js
@@ -1,11 +1,11 @@
 import optionsJson from './options.json' with {type: 'json'};
 
-import { StartFunc as StartFuncOnClickRowFunc } from "./onClickRow/EntryFile.js";
+import { StartFunc as ForColumns } from "./ForColumns/entryFile.js";
 
 const StartFunc = () => {
     var $table = $('#table');
 
-    optionsJson.onClickRow = StartFuncOnClickRowFunc;
+    ForColumns({ inColumns: optionsJson.columns });
 
     $table.bootstrapTable(optionsJson);
 };
