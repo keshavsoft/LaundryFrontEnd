1a59f9ea66d912f9109e30824a2dafcbb96effb7
sharath0036 <ksharathbabu2@gmail.com>
2024-11-01 17:53:25 +0530

    short removed

--- src/Laundry/Admin/AdminAllDays/QrCodeWise/BranchWise/SourceCode/Js/pages/ToFactoryShort/FormLoad/BuildBsTable/onClickRow/EntryFile.js
+++
@@ -1,8 +0,0 @@
-import { StartFunc as StartFuncButtonClickFunc } from "./QRGenerate/2-ButtonClickFunc.js";
-
-const StartFunc = (row, $element, field) => {
-    if (field === "QRRaise") {
-        StartFuncButtonClickFunc({ inRowData: row });
-    };
-};
-export { StartFunc };
