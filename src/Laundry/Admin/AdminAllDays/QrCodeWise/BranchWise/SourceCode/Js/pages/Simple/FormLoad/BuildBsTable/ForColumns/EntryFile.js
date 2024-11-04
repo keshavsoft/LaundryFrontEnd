5ab12279beb9eae4b6026c1157cf11cc6718aa98
sharath0036 <ksharathbabu2@gmail.com>
2024-11-04 19:14:04 +0530

    added new column

--- src/Laundry/Admin/AdminAllDays/QrCodeWise/BranchWise/SourceCode/Js/pages/Simple/FormLoad/BuildBsTable/ForColumns/EntryFile.js
+++ src/Laundry/Admin/AdminAllDays/QrCodeWise/BranchWise/SourceCode/Js/pages/Simple/FormLoad/BuildBsTable/ForColumns/EntryFile.js
@@ -1,11 +1,13 @@
 import { StartFunc as ColumnOperate } from "./ColumnSerial/entryFile.js";
 import { StartFunc as ColumnRate } from "./ColumnRate/entryFile.js";
+import { StartFunc as ColumnScan } from "./ColumnScan/entryFile.js";
 
 
 let StartFunc = ({ inColumns }) => {
     let LocalColumns = inColumns;
     let LocalColumnOperateFine = LocalColumns.find(element => element.field === "KS-Serial");
     let LocalColumnRate = LocalColumns.find(element => element.field === "Rate");
+    let LocalQrShow = LocalColumns.find(element => element.field === "QrShow");
 
 
     if (LocalColumnOperateFine === undefined === false) {
@@ -13,6 +15,9 @@ let StartFunc = ({ inColumns }) => {
     };
     if (LocalColumnRate === undefined === false) {
         ColumnRate({ inFindColumn: LocalColumnRate });
+    };
+    if (LocalQrShow === undefined === false) {
+        ColumnScan({ inFindColumn: LocalQrShow });
     };
 };
 
