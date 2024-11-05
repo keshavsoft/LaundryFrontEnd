2114f896099f5996a62b2cd7237729fca580c0b7
Nalipi Sridhar Reddy <nalipisridharreddy@gmail.com>
2024-11-05 12:21:16 +0530

    Branch filter

--- src/Laundry/Admin/AdminAllDays/QrCodeWise/BranchFilter/SourceCode/Js/pages/statusFilterShow/FormLoad/AddListeners/RefreshBSTableId/Receipts/PostFetch.js
+++ src/Laundry/Admin/AdminAllDays/QrCodeWise/BranchFilter/SourceCode/Js/pages/statusFilterShow/FormLoad/AddListeners/RefreshBSTableId/Receipts/PostFetch.js
@@ -2,11 +2,19 @@ import ConfigJson from '../../../../Config.json' with {type: 'json'};
 
 let StartFunc = async () => {
     let jVarLocalFetchUrl = ConfigJson.GetUrl;
+    let jVarLocalBranchName = getUrlQueryParams({ inGetKey: "BranchName" });
 
-    let response = await fetch(jVarLocalFetchUrl);
+
+    let response = await fetch(`${jVarLocalFetchUrl}/${jVarLocalBranchName}`);
 
     return await response;
 };
 
+let getUrlQueryParams = ({ inGetKey }) => {
+    const queryString = window.location.search;
+    const parameters = new URLSearchParams(queryString);
+    const value = parameters.get(inGetKey);
+    return value;
+};
 export { StartFunc };
 
