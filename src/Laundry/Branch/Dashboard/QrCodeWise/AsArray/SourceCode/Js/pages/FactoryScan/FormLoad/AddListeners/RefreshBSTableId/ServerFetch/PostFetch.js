c259b6465f0fe75e05d52b01e894f08e4ae0d229
pushpadamalanka82 <y*********@gmail.com>
2024-11-08 16:39:37 +0530

    errors

--- src/Laundry/Branch/Dashboard/QrCodeWise/AsArray/SourceCode/Js/pages/FactoryScan/FormLoad/AddListeners/RefreshBSTableId/ServerFetch/PostFetch.js
+++ src/Laundry/Branch/Dashboard/QrCodeWise/AsArray/SourceCode/Js/pages/FactoryScan/FormLoad/AddListeners/RefreshBSTableId/ServerFetch/PostFetch.js
@@ -5,7 +5,7 @@ let StartFunc = async () => {
     //const jVarLocalBranchName = localStorage.getItem("BranchName");
     
     // let jVarLocalFetchUrl = /${urlJson.StartRoute}/${jVarLocalBranchName}/Show/DataOnly;
-    let jVarLocalBranchName = getUrlQueryParams({ inGetKey: "BranchName" });
+    let jVarLocalBranchName = localStorage.getItem("BranchName");
 
     let jVarLocalFetchUrl = ConfigJson.GetUrl;
     
@@ -13,13 +13,6 @@ let StartFunc = async () => {
 
     let response = await fetch(`/${jVarLocalFetchUrl}/${jVarLocalBranchName}`);
     return await response;
-};
-
-let getUrlQueryParams = ({ inGetKey }) => {
-    const queryString = window.location.search;
-    const parameters = new URLSearchParams(queryString);
-    const value = parameters.get(inGetKey);
-    return value;
 };
 
 export { StartFunc };
