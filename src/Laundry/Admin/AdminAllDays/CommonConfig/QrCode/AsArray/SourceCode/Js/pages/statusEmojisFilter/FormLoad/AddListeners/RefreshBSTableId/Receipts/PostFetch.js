458479875ed29fbf82e2c33c8053103df023f733
sharath0036 <ksharathbabu2@gmail.com>
2024-11-14 12:23:11 +0530

    commonconfig AsArray UI

--- src/Laundry/Admin/AdminAllDays/CommonConfig/QrCode/AsArray/SourceCode/Js/pages/statusEmojisFilter/FormLoad/AddListeners/RefreshBSTableId/Receipts/PostFetch.js
+++ src/Laundry/Admin/AdminAllDays/CommonConfig/QrCode/AsArray/SourceCode/Js/pages/statusEmojisFilter/FormLoad/AddListeners/RefreshBSTableId/Receipts/PostFetch.js
@@ -1,9 +1,11 @@
 import ConfigJson from '../../../../Config.json' with {type: 'json'};
+import CommonConfig from '../../../../../CommonConfig.json' with {type: 'json'};
 
 let StartFunc = async () => {
-    let jVarLocalFetchUrl = ConfigJson.GetUrl;
+    let jVarLocalGetStartUrl = CommonConfig.GetStartUrl;
+    let jVarLocalGetEndPoint = ConfigJson.GetEndPoint;
 
-    let response = await fetch(jVarLocalFetchUrl);
+    let response = await fetch(`${jVarLocalGetStartUrl}/${jVarLocalGetEndPoint}`);
 
     return await response;
 };
